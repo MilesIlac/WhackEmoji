5d6113b3b58127ce1ef2e32d584f06fffcfcb301
MilesIlac <smile.caliprf97@gmail.com>
2022-01-06 19:49:29 +0800

    Updated Credits dialog

--- app/src/main/java/com/milesilac/whackamolu/MenuActivity.java
+++ app/src/main/java/com/milesilac/whackamolu/MenuActivity.java
@@ -5,6 +5,7 @@ import androidx.core.content.ContextCompat;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,7 +13,7 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button btnQuickPlay, btnCustomPlay, btnScore, btnCredits, btnQuit, btnCustomConfirmPlay, btnCustomBack;
+    Button btnQuickPlay, btnCustomPlay, btnScore, btnCredits, btnQuit, btnCustomConfirmPlay, btnCustomBack, btnGit;
     Dialog menuCustomPlay, menuScore, menuCredits;
 
     @Override
@@ -27,6 +28,7 @@ public class MenuActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+
         //TODO create Custom dialog parameters logic (editText,radioGroup,etc)
         menuCustomPlay = new Dialog(this);
         menuCustomPlay.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -38,6 +40,7 @@ public class MenuActivity extends AppCompatActivity {
         btnCustomConfirmPlay = menuCustomPlay.findViewById(R.id.btnPlay);
         btnCustomBack = menuCustomPlay.findViewById(R.id.btnBack);
 
+        //show custom game menu
         btnCustomPlay.setOnClickListener(v -> menuCustomPlay.show());
 
         //TODO edit intent.putExtra();
@@ -47,6 +50,7 @@ public class MenuActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        //close custom game menu
         btnCustomBack.setOnClickListener(v -> menuCustomPlay.dismiss());
 
 
@@ -57,23 +61,33 @@ public class MenuActivity extends AppCompatActivity {
         //menuCustomPlay.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_background));
         menuScore.setCancelable(false);
 
-        btnScore = menuScore.findViewById(R.id.menubtn3);
+        btnScore = findViewById(R.id.menubtn3);
 
-        //TODO create Credits dialog, edit .setContentView
+
         menuCredits = new Dialog(this);
         menuCredits.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //menuCustomPlay.setContentView(R.layout.output);
-        //menuCustomPlay.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_background));
+        menuCredits.setContentView(R.layout.credits_dialog);
+        menuCredits.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_background));
         menuCredits.setCancelable(false);
 
-        btnCredits = menuCredits.findViewById(R.id.menubtn4);
+        btnCredits = findViewById(R.id.menubtn4);
+        btnGit = menuCredits.findViewById(R.id.btnGit);
+        btnCustomBack = menuCredits.findViewById(R.id.btnBack);
+
+        //show credits
+        btnCredits.setOnClickListener(v -> menuCredits.show());
+
+        //view Github in local browser
+        btnGit.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/MilesIlac/whack-a-molu"));
+            startActivity(browserIntent);
+        });
+
+        //close credits
+        btnCustomBack.setOnClickListener(v -> menuCredits.dismiss());
+
 
         btnQuit = findViewById(R.id.menubtn5);
-        btnQuit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        btnQuit.setOnClickListener(v -> finish());
     }
 }
